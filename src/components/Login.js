5bc7c7f5ab075c9687288b3fd9e2e6a7686c7772
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2022-03-05 16:58:46 -0300

    Adicione e estilize imagem e texto na área principal da página de login

--- src/components/Login.js
+++ src/components/Login.js
@@ -12,6 +12,18 @@ const Login = (props) => {
                     <SignIn>Join Now</SignIn>
                 </div>
             </Nav>
+            <Section>
+                <Hero>
+                     <h1>welcome to your professional community</h1>
+                     <img src="./images/login-hero.svg" alt="" />
+                </Hero>
+                <Form>
+                     <Google>
+                         <img src="/images/google.svg" alt="" />
+                         Sign in With Google
+                     </Google>
+                </Form>
+            </Section>
         </Container>
     );
 }
@@ -70,5 +82,56 @@ background-color: rgba(0, 0, 0, 0);
     color: #0a66c2;
 }
 `;
+
+const Section =  styled.section `
+ display: flex;
+ align-content: flex-start;
+ min-height: 700px;
+ padding-bottom: 138px;
+ padding-top: 40px;
+ padding: 60px 0;
+ position: relative;
+ flex-wrap: wrap;
+ width: 100%;
+ max-width: 1128px;
+ align-items: center;
+ margin: auto;
+ @media (max-width: 768) {
+     margin: auto;
+     min-height: 0px;
+ }
+`;
+
+const Hero = styled.div `
+width: 100%;
+h1 {
+    padding-bottom: 0;
+    width: 55%;
+    font-size: 56px;
+    color: #2977c9;
+    font-weight: 200;
+    line-height: 70px;
+    @media (max-width: 768px) {
+        text-align: center;
+        font-size: 20px;
+        width: 100%;
+        line-height: 2;
+    }
+}
+
+img {
+    z-index: -1;
+    width: 700px;
+position: absolute;
+bottom: -2px;
+right: -150px;
+@media (max-width: 768px) {
+    top: 230px;
+    width: initial;
+    position: initial;
+    height: initial;
+}
+}
+`
 
 export default Login
