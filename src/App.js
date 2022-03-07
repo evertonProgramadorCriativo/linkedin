47e5951b49dab0044d41cbc08ae185163c784e5c
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2022-03-07 17:04:09 -0300

    Adicionando o anúncio UpWork no topo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
  import Login from "./components/Login"
  import { BrowserRouter as Router , Routes ,  Route } from "react-router-dom"
 import './App.css';  
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -8,8 +10,10 @@ function App() {
        <Router>
          <Routes>
             <Route exact path="/" element={<Login/>}   />
-                 
-             
+         </Routes>
+         <Routes>
+            <Route  path="/home" element={<><Header/> <Home /></>}    />
+           
          </Routes>
        </Router>
     </div>
