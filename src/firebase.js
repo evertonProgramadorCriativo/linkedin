5446925adab92d471d72c4903c58400feabbcb67
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2022-03-22 22:53:12 -0300

    tirando o anuncio do firabase do site

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,18 @@ import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 import "firebase/compat/firestore"
 import "firebase/compat/storage"
+ 
+ 
+
+ 
 const firebaseConfig = {
-    apiKey: "AIzaSyDk0FMdQ8y_JtnpbgrI22mXnLKk9To44RY",
-    authDomain: "linkedin-d95f2.firebaseapp.com",
-    projectId: "linkedin-d95f2",
-    storageBucket: "linkedin-d95f2.appspot.com",
-    messagingSenderId: "251965596526",
-    appId: "1:251965596526:web:ad77e7805ab5565f2b177f"
+  apiKey : "AIzaSyDk0FMdQ8y_JtnpbgrI22mXnLKk9To44RY" ,
+  authDomain : "linkedin-d95f2.firebaseapp.com" ,
+  projectId : "linkedin-d95f2" ,
+  storageBucket : "linkedin-d95f2.appspot.com" ,
+  messagingSenderId : "251965596526" ,
+  appId : "1:251965596526:web:ad77e7805ab5565f2b177f"
+   
   };
  
   const firebaseApp = firebase.initializeApp(firebaseConfig);
